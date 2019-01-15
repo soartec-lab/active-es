d7656424383394297e0fb1714710f8bdb8dcf64f
soartec-lab <info@soartec-lab.work>
2019-01-16 00:20:32 +0900

    create def multi_match

--- lib/active_search/quering.rb
+++ lib/active_search/quering.rb
@@ -29,5 +29,32 @@ module ActiveSearch
         new(hit["_source"].merge({ id: hit["_id"], score: hit["_score"] }))
       end
     end
+
+    # usingd
+    # ActiveSearch::Base.multi_match(field: ['title', 'description'], match: 'sample 1')
+    # => [#<Content:0x00007fffbc2746b0 @description="sample description 1", @id="W_KDRmgBeTay2K79iAf7", @score=0.5753642, @title="sample title 1">]
+    #
+    # ActiveSearch::Base.multi_match(fields: ['title', 'description'], keyword: ['sample', '1'])
+    # => [#<Content:0x00007fffbc2746b0 @description="sample description 1", @id="W_KDRmgBeTay2K79iAf7", @score=0.5753642, @title="sample title 1">]
+    #
+    # ActiveSearch::Base.multi_match(fields: ['title', 'description'], match: ['sample', '1'], operator: 'or')
+    # => [
+    #      #<Content:0x00007ffff712d280 @description="sample description 1", @id="W_KDRmgBeTay2K79iAf7", @score=0.5753642, @title="sample title 1">,
+    #      #<Content:0x00007ffff712c6a0 @description="sample description 2", @id="XPKDRmgBeTay2K79ywfz", @score=0.2876821, @title="sample title 2">
+    #    ]
+    #
+    # ActiveSearch::Base.multi_match(fields: '', match: [])
+    # => []
+    def multi_match(fields:, match:, operator: 'and')
+      raise ArgumentError, "operator accepts only 'and','or'" unless operator =~ /and|or/
+
+      keyword = Array(match).join(' ')
+      body = { query: { multi_match: { fields: fields, query: keyword, operator: operator} } }
+      result = client.search index: index, type: type, body: body
+
+      result.dig("hits", "hits").map do |hit|
+        new(hit["_source"].merge({ id: hit["_id"], score: hit["_score"] }))
+      end
+    end
   end
 end
