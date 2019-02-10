3cf99fe64ab58806192d4ebdda85b809dee76b9a
soartec-lab <info@soartec-lab.work>
2019-02-10 18:45:08 +0900

    propety define in Content

--- spec/dummy/app/models/content.rb
+++ spec/dummy/app/models/content.rb
@@ -1,4 +1,6 @@
 class Content < ActiveSearch::Base
   property :title, type: 'text', analyzer: 'kuromoji'
   property :description, type: 'text', analyzer: "kuromoji"
+  property :number, type: 'integer'
+  property :rank, type: 'integer'
 end
